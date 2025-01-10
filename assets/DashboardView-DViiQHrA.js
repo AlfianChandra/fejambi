65411075d6f95dfa9c8de60246e6a08ea792622b
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-01-10 09:15:25 +0700

    initial
    initial commit

---
+++ assets/DashboardView-DViiQHrA.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./MainWrapperView.vue_vue_type_style_index_0_lang-BcW0l8sl.js";import{d as e,o as t,f as s}from"./index-UmRKcWIK.js";import"./theme-MMYVCpC6.js";const m=e({__name:"DashboardView",setup(n){return(i,a)=>(t(),s(o,{"context-title":"Finding Oil Losses","context-desc":"https://findingoillosses.com"}))}});export{m as default};
