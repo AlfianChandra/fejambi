9f3816ae6205ccace673f0f918a2b8b4ba9646ac
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-01-17 08:48:57 +0700

    -
    -

---
+++ assets/DashboardView-DyWR7ATi.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./MainWrapperView.vue_vue_type_style_index_0_lang-YUWgPjPW.js";import{d as e,o as t,f as s}from"./index-BmlK4ydT.js";import"./theme-ChZ7ZSSf.js";const m=e({__name:"DashboardView",setup(n){return(i,a)=>(t(),s(o,{"context-title":"Finding Oil Losses","context-desc":"https://findingoillosses.com"}))}});export{m as default};
